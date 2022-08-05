5be58f7a81027870086c9d597bd357b4de8008d6
boomi1709 <boomibalan7766@gmail.com>
2022-08-05 12:17:56 +0530

    init

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import wave from '../img/Emoji.png'
-import me from '../img/me.jpg'
+import Image from '../img/44400.jpg'
 
 function Content() {
   return (
@@ -8,9 +8,10 @@ function Content() {
       <main>
         <div>
           <h1>Hi <img src={wave} alt="wave" width={40} /></h1>
-          <h1>I'm Charles</h1>
+          <h1>I'm Boomibalan.D</h1>
           <h1>Front-end developer</h1>
-          <p>I design and developer experiences that make people's lives simpler through Web and Mobile Apps. I work with Figma, HTML5, CSS3, JavaScript, React, ReactNative and Flutter</p>
+          <p>I design and developer experiences that make people's lives simpler through Web and Mobile Apps.
+             I work with Figma, HTML5, CSS3, JavaScript, React, ReactNative and Flutter</p>
         </div>
         <div className='buttons'>
           <button className="cta hire">HIRE ME</button>
@@ -18,7 +19,7 @@ function Content() {
         </div>
       </main>
       <figure>
-        <img src={me} alt="Charles Kasasira" className='me-img' width={350} />
+        <img src={Image} alt="Charles Kasasira" className='me-img' width={350} />
         <div className='img-bg'></div>
       </figure>
     </div>
